b8adab856259ee84a4f8cb0bab4470cabc271976
Lucas Guo <guoliubin@gmail.com>
2018-07-08 17:53:22 +0800

    Follow update instruction and add data-widget

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -4,62 +4,83 @@
 
         <!-- Sidebar user panel -->
         <div class="user-panel">
-            <div class="pull-left image">
-                <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
-            </div>
-            <div class="pull-left info">
-                <p>Alexander Pierce</p>
-
-                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-            </div>
+			<div class="site-logo">
+                <img src="images/logo.png"/>
+			</div>
+			<div class="site-logo-mini">
+                <img src="images/logo-mini.png"/>
+			</div>
         </div>
-
-        <!-- search form -->
-        <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-                <input type="text" name="q" class="form-control" placeholder="Search..."/>
-              <span class="input-group-btn">
-                <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-            </div>
-        </form>
-        <!-- /.search form -->
 
         <?= dmstr\widgets\Menu::widget(
             [
-                'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
+                'options' => ['class' => 'sidebar-menu tree', 'data-widget' => 'tree'],
                 'items' => [
-                    ['label' => 'Menu Yii2', 'options' => ['class' => 'header']],
-                    ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii']],
-                    ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug']],
-                    ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
+                    ['label' => '功能菜单', 'options' => ['class' => 'header']],
+
+		            ['label' => '我的客户', 'icon' => 'star', 'url' => ['/customer/index'], 'visible' => Yii::$app->user->can('addCustomer')],
+
+                	[
+                		'label' => '客户管理', 
+                		'icon' => 'search-plus', 
+                		'url' => '#',
+                		'visible' => (Yii::$app->user->can('updateCustomer') || Yii::$app->user->can('maintainDealerShop') || Yii::$app->user->can('viewAllCardBuyer')),
+                		'items' => [
+                			['label' => '客户管理', 'url' => ['/customer/all-index'], 'visible' => Yii::$app->user->can('updateCustomer')],
+                            ['label' => '办卡客户管理', 'url' => ['/card-buyer/all-index'], 'visible' => Yii::$app->user->can('viewAllCardBuyer')],
+                   			['label' => '经销商门店管理', 'url' => ['/dealer-shop/index'], 'visible' => Yii::$app->user->can('maintainDealerShop')],
+                		],
+                	],
                     [
-                        'label' => 'Some tools',
-                        'icon' => 'share',
+                        'label' => '门店管理',
+                        'icon' => 'home',
                         'url' => '#',
+                        'visible' => (Yii::$app->user->can('addCardBuyer') || Yii::$app->user->can('viewOwnCardBuyer') || Yii::$app->user->can('addEmployee')),
                         'items' => [
-                            ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii'],],
-                            ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug'],],
-                            [
-                                'label' => 'Level One',
-                                'icon' => 'circle-o',
-                                'url' => '#',
-                                'items' => [
-                                    ['label' => 'Level Two', 'icon' => 'circle-o', 'url' => '#',],
-                                    [
-                                        'label' => 'Level Two',
-                                        'icon' => 'circle-o',
-                                        'url' => '#',
-                                        'items' => [
-                                            ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
-                                            ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
-                                        ],
-                                    ],
-                                ],
-                            ],
+                            ['label' => '添加顾客', 'url' => ['/card-buyer/create'], 'visible' => Yii::$app->user->can('addCardBuyer')],
+                            ['label' => '顾客列表', 'url' => ['/card-buyer/index'], 'visible' => Yii::$app->user->can('viewOwnCardBuyer') || Yii::$app->user->can('addCardUsage')],
+                            ['label' => '员工管理', 'url' => ['/employee/index'], 'visible' => Yii::$app->user->can('addEmployee')],
+                            ['label' => '门店统计', 'url' => ['/card-buyer/info'], 'visible' => Yii::$app->user->can('viewOwnCardBuyer')],
                         ],
                     ],
+                	[
+	                	'label' => '仪器管理',
+	                	'icon' => 'cubes',
+	                	'url' => '#',
+	                	'visible' => Yii::$app->user->can('manageMachine'),
+	                	'items' => [
+	                		['label' => '仪器管理', 'url' => ['/machine/index'], 'visible' => Yii::$app->user->can('manageMachine')],
+	                		['label' => '订单出库', 'url' => ['/order/out-index'], 'visible' => Yii::$app->user->can('manageMachine')],
+	                		['label' => '配件管理', 'url' => ['/part/index'], 'visible' => Yii::$app->user->can('manageMachine')],
+	                	],
+                	],
+                	['label' => '订单管理', 'icon' => 'file-text', 'url' => ['/order/index'], 'visible' => (Yii::$app->user->can('addOrder') || Yii::$app->user->can('updateOrder'))],
+                    ['label' => '文章管理', 'icon' => 'book', 'url' => ['/article/index'], 'visible' => (Yii::$app->user->can('maintainArticle'))],
+                    ['label' => '用户管理', 'icon' => 'users', 'url' => ['/user/index'], 'visible' => Yii::$app->user->can('manageUser')],
+                	[
+	                	'label' => '财务管理',
+	                	'icon' => 'money',
+	                	'url' => '#',
+	                	'visible' => (Yii::$app->user->can('addFinance') || Yii::$app->user->can('updateFinance') || Yii::$app->user->can('viewFinance')),
+	                	'items' => [
+                            ['label' => '收支情况(新)', 'url' => ['/finance/default/index', 'isNew' => true], 'visible' => (Yii::$app->user->can('addFinance') || Yii::$app->user->can('updateFinance'))],
+                            ['label' => '财务报表(新)', 'url' => ['/finance/default/chart', "isNew" => true], 'visible' => Yii::$app->user->can('viewFinance')],
+	                		['label' => '收支情况', 'url' => ['/finance/default/index'], 'visible' => (Yii::$app->user->can('addFinance') || Yii::$app->user->can('updateFinance'))],
+	                		['label' => '财务报表', 'url' => ['/finance/default/chart'], 'visible' => Yii::$app->user->can('viewFinance')],
+	                	],
+                	],
+                	['label' => '内部文件', 'icon' => 'file-word-o', 'url' => ['/meeting-record/index'], 'visible' => Yii::$app->user->can('meetingRecord')],
+                	[
+	                	'label' => '系统设置',
+	                	'icon' => 'gears',
+	                	'url' => '#',
+	                	'visible' => Yii::$app->user->can('manageProduct'),
+	                	'items' => [
+	                		['label' => '产品管理', 'url' => ['/product/index'], 'visible' => Yii::$app->user->can('manageProduct')],
+	                		['label' => '配件类型管理', 'url' => ['/part-type/index'], 'visible' => Yii::$app->user->can('manageProduct')],
+	                		['label' => '理疗卡类型管理', 'url' => ['/card-type/index'], 'visible' => Yii::$app->user->can('maintainCardType')],
+	                	],
+                	],
                 ],
             ]
         ) ?>

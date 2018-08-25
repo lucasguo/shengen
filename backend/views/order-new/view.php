74a2e73d8e346cd8a84968fcc6b9ab1d7dd60777
Lucas Guo <guoliubin85@foxmail.com>
2018-08-25 23:06:06 +0800

    fix for old data

--- backend/views/order-new/view.php
+++ backend/views/order-new/view.php
@@ -40,7 +40,7 @@ $this->params['breadcrumbs'][] = $this->title;
         ],
         [
             'attribute' => 'hospital_id',
-            'value' => $model->hospital->hospital_name,
+            'value' => $model->hospital ? $model->hospital->hospital_name : null,
         ],
         'office',
         [
